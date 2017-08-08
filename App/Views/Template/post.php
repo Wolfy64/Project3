3c857da1a74a09ab66dec25c790389512410385c
Wolfy64 <ledavid64@gmail.com>
2017-08-09 00:46:29 +0200

    Modidy post.php

--- App/Views/Template/post.php
+++ App/Views/Template/post.php
@@ -1,13 +1,17 @@
-<?php $this->title = 'Mon Blog - ' . $title ?>
+<?php $this->title = 'Mon Blog - ' . $data['blogPost']['title'] ?>
+
+<?php var_dump($data)  ?>
 
 <article>
     <h1> <?= $this->title ?> </h1>
-    <?= $contents ?>
-    <time> <?= $post['dateContents'] ?> </time>
-    <p> <?= $post['contents'] ?> </p>
+    <time> <?= $data['blogPost']['dateContents'] ?> </time>
+    <p> <?= $data['blogPost']['contents'] ?> </p>
 </article>
 <hr>
-<h1>Réponse à <?= $post['title'] ?></h1>
+<h2>Réponse à <?= $data['blogPost']['title'] ?></h2>
+
+<?php $data['blogComments'] ?>
+
 <?php foreach ( $comments as $comment ): ?>
     <p> <?= $comment['author'] ?></p>
     <p> <?= $comment['contents'] ?></p>
