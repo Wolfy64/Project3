e1662bcf96f54236a62601407bbd1e2629880d41
Wolfy64 <ledavid64@gmail.com>
2017-08-18 11:55:45 -0700

    Add line in .htaccess

--- App/Controllers/Admin.php
+++ App/Controllers/Admin.php
@@ -4,6 +4,11 @@ class Admin
 {
     public function __construct()
     {
+        $this->loadAction();
+    }
+
+    public function loadAction()
+    {
         echo '<h1>Page Admin</h1>';
     }
 }
