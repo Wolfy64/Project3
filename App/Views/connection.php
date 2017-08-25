809444c2e1ca773f621bc7e738ad96e903060704
Wolfy64 <ledavid64@gmail.com>
2017-08-25 16:56:04 -0700

    Minor fix in connection.php

--- App/Views/connection.php
+++ App/Views/connection.php
@@ -1,7 +1,14 @@
-<form action="connection" method="POST">
-    <label for="user">User</label>
-    <input type="text" name="user" value="">
-    <label for="password">Password</label>
-    <input type="password" name="password" value="">
-    <button type="submit">Connection</button>
+<form action="/connection" method="POST">
+    <fieldset>
+        <legend>Space Administrators</legend>
+        <p>
+            <label for="user">User</label>
+            <input type="text" name="user" required>
+        </p>
+        <p>
+            <label for="password">Password</label>
+            <input type="password" name="password" required>
+        </p>
+        <button type="submit">Connection</button>
+    </fieldset>
 </form>
