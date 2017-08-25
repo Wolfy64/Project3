12f0efc1b47f887dd10645098561084f6648da0e
Wolfy64 <ledavid64@gmail.com>
2017-08-25 10:00:23 -0700

    Add Comment field in alaskaPost.php

--- App/Views/alaskaPost.php
+++ App/Views/alaskaPost.php
@@ -11,7 +11,7 @@
     <p> Published on <?= $post->getDateContents() ?> </p>
     <p>              <?= $post->getAuthor();      ?> </p>
     <p>              <?= $post->getContents();    ?> </p>
-    <p><a href="#">Add Comment</a></p>
+    <p> <a href="#comment">Add Comment</a></p>
 </div>
 
 <!-- Show Comments -->
@@ -19,10 +19,28 @@
     <?php foreach ($post->getCommentsList() as $comment) { ?>
 
 <div>
-    <p><?= $comment->getTitle() ?></p>
-    <p><?= $comment->getDateContents() ?></p>
-    <p><?= $comment->getAuthor() ?></p>
-    <p><?= $comment->getContents() ?></p>
+    <p> <?= $comment->getTitle()        ?> </p>
+    <p> <?= $comment->getDateContents() ?> </p>
+    <p> <?= $comment->getAuthor()       ?> </p>
+    <p> <?= $comment->getContents()     ?> </p>
 </div>
 
     <?php } ?>
+
+<!-- Add Comments -->
+
+<form action="/addComment" method="post">
+  <fieldset id="comment">
+    <legend>Something to say?</legend>
+    <p>
+        <label>Nickname</label><br>
+        <input type="text" name="nickname" placeholder="e.g Alex" required>
+    </p>
+    <p>
+        <label>Your message</label><br>
+        <textarea rows="5" cols="40" placeholder="e.g My name is Alex" required></textarea>
+    </p>
+    <button type="submitt">Add your comment</button>
+    <button type="reset">Reset</button>
+  </fieldset>
+</form>
