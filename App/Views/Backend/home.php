3884793e43985b13f37dbcb3e22f1c097bc35d45
Wolfy64 <ledavid64@gmail.com>
2017-09-10 16:30:03 -0700

    fix responsive dashboard

--- App/Views/Backend/home.php
+++ App/Views/Backend/home.php
@@ -1,99 +1,103 @@
-<div class="container">
-    
-    <div class="row">
+<div clas="view hm-black-strong">
+    <div class="full-bg-img flex center">
+        <div class= "container">
 
-        <div class="col-12">
-            
-            <!-- Report Comment -->
-            <a href="manageReport">
+            <div class="row ">
 
-                <button type="button" class="btn-admin amber">
-                    <p>
-                        <i class="fa fa-exclamation-circle fa-3x"></i>
-                    </p>
-                    <p>
-                        <?= $data['reportCount'] ?> comment(s) reported
-                    </p>
-                </button>
+                <div class="col-lg-12 text-center">
+                    
+                    <!-- Report Comment -->
+                    <a href="manageReport">
+
+                        <button type="button" class="btn-admin amber">
+                            <p>
+                                <i class="fa fa-exclamation-circle fa-3x"></i>
+                            </p>
+                            <p>
+                                <?= $data['reportCount'] ?> comment(s) reported
+                            </p>
+                        </button>
+                        
+                    </a>
                 
-            </a>
-        
-            <!-- Show Post -->
-            <a href="managePost">
-        
-                <button type="" class="btn-admin green">
-                    <p>
-                        <i class="fa fa-newspaper-o fa-3x"></i>
-                    </p>
-                    <p>
-                        <?= $data['postCount'] ?> posts published
-                    </p>
-                </button>
-        
-            </a>
-        
-            <!-- Show Comment -->
-            <a href="manageComment">
-        
-                <button type="" class="btn-admin blue">
-                    <p>
-                        <i class="fa fa-comment-o fa-3x"></i>
-                    </p>
-                    <p>
-                        <?= $data['commentCount'] ?> users comment
-                    </p>
-                </button>
-        
-            </a>
-    
-        </div>
-    
-        <div class="col-12">
-    
-            <!-- New Post -->
-            <a href="writePost">
-        
-                <button type="" class="btn-admin indigo">
-                    <p>
-                        <i class="fa fa-pencil fa-3x"></i>
-                    </p>
-                    <p>
-                        Write a post
-                    </p>
-                </button>
-        
-            </a>
-        
-            <!-- Settings -->
-            <a href="settings">
-        
-                <button type="" class="btn-admin blue-grey">
-                    <p>
-                        <i class="fa fa-cog fa-3x"></i>
-                    </p>
-                    <p>
-                        Settings
-                    </p>
-                </button>
-        
-            </a>
-        
-            <!-- Sign Out -->
-            <a href="/admin/signOut">
-        
-                <button type="" class="btn-admin red">
-                    <p>
-                        <i class="fa fa-sign-out fa-3x"></i>
-                    </p>
-                    <p>
-                        Sign Out
-                    </p>
-                </button>
-        
-            </a>
+                    <!-- Show Post -->
+                    <a href="managePost">
+                
+                        <button type="" class="btn-admin green">
+                            <p>
+                                <i class="fa fa-newspaper-o fa-3x"></i>
+                            </p>
+                            <p>
+                                <?= $data['postCount'] ?> posts published
+                            </p>
+                        </button>
+                
+                    </a>
+                
+                    <!-- Show Comment -->
+                    <a href="manageComment">
+                
+                        <button type="" class="btn-admin blue">
+                            <p>
+                                <i class="fa fa-comment-o fa-3x"></i>
+                            </p>
+                            <p>
+                                <?= $data['commentCount'] ?> users comment
+                            </p>
+                        </button>
+                
+                    </a>
+            
+                </div>
+            
+                <div class="col-lg-12 text-center">
+            
+                    <!-- New Post -->
+                    <a href="writePost">
+                
+                        <button type="" class="btn-admin indigo">
+                            <p>
+                                <i class="fa fa-pencil fa-3x"></i>
+                            </p>
+                            <p>
+                                Write a post
+                            </p>
+                        </button>
+                
+                    </a>
+                
+                    <!-- Settings -->
+                    <a href="settings">
+                
+                        <button type="" class="btn-admin blue-grey">
+                            <p>
+                                <i class="fa fa-cog fa-3x"></i>
+                            </p>
+                            <p>
+                                Settings
+                            </p>
+                        </button>
+                
+                    </a>
+                
+                    <!-- Sign Out -->
+                    <a href="/admin/signOut">
+                
+                        <button type="" class="btn-admin red">
+                            <p>
+                                <i class="fa fa-sign-out fa-3x"></i>
+                            </p>
+                            <p>
+                                Sign Out
+                            </p>
+                        </button>
+                
+                    </a>
+                    
+                </div>
+                
+            </div>
             
         </div>
-        
     </div>
-
 </div>
